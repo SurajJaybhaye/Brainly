180af0c77190681b1f29a13c49a0371be6717ed6
SurajJaybhaye <101545931+SurajJaybhaye@users.noreply.github.com>
2025-11-16 01:33:45 +0530

    Updatedd

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,1 @@
 export const JWT_PASSWORD = "123123";
-// jaybhayesuraj7_db_user
-// 1egsGSkTtyUZR5Cz
-// mongodb+srv://jaybhayesuraj7_db_user:1egsGSkTtyUZR5Cz@cluster0.y0jvtz1.mongodb.net/?appName=Cluster0
